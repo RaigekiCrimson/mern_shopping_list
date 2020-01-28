7de557d1afde1b737c016edb185f954150265f47
Will Parson II <RaigekiCrimson@outlook.com>
2020-01-27 23:52:44 -0500

    Finished Backend
    Backend complete, and connected to postman
    started client-side work with react

--- routes/api/items.js
+++ routes/api/items.js
@@ -13,4 +13,24 @@ router.get('/',  (req, res) => {    // we are already in /api/item route so we j
     .then(items => res.json(items))
 }); 
 
+// @route POST api/items
+// @description Create an Item
+// @access Public
+router.post('/',  (req, res) => {    // we are already in /api/item route so we just need a forward slash
+    const newItem = new Item({
+        name: req.body.name // body parser allows this
+    });
+
+    newItem.save().then(item => res.json(item));
+}); 
+
+// @route DELETE api/items/:id
+// @description Delete a Item
+// @access Public
+router.delete('/:id',  (req, res) => {    
+    Item.findById(req.params.id)
+    .then(item => item.remove().then(() => res.json({success: true})))
+    .catch(err => res.status(404).json({success: false}));
+})
+
 module.exports = router;
